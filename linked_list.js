972fc3c93ffa3d23594bb3d3191e5cc103c2f2d9
Halima <just.hvli@gmail.com>
2025-01-30 13:36:44 +0100

    correction

--- linked_list.js
+++ linked_list.js
@@ -1,18 +1,22 @@
 class Node {
     // An object for storing a single node of a linked list
     // Models two attributes - data and the link to the next node in the list
-    data = null;
-    nextNode = null;
-
     constructor(data) {
         this.data = data;
+        this.nextNode = null;  // On initialise nextNode dans le constructor
     }
 
-    repr() {
-        return "<Node data=" + this.data + ">";
+    toString() {  // En JavaScript, on utilise toString() plutôt que repr()
+        return `<Node data=${this.data}>`; // Template literal pour plus de lisibilité
     }
 }
 
-let node1 = new Node(1);
+let node1 = new Node(10);
+let node2 = new Node(20);
+let node3 = new Node(30);
 
-console.log(node1.repr());
+node1.nextNode = node2;
+node2.nextNode = node3;
+
+console.log(node2.nextNode.data);
+
